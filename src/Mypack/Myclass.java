ff07812462932d0cc83536cdb81dd2ec2f4ddd55
manvika123 <manvikajaiswal1995@gmail.com>
2017-09-16 02:19:58 +0530

    first commit

---
+++ src/Mypack/Myclass.java
@@ -0,0 +1,14 @@
+package Mypack;
+
+public class Myclass
+
+{
+    public static void main(String args[])
+    {
+
+        System.out.println("Welcome to Java, Welcome to computer science and programming is fun");
+    }
+}
+
+
+
